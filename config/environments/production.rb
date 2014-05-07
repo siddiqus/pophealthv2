9481790f534ac6d36e988021916fb43334caad09
Sabbir Siddiqui <ssiddiqui@gosmcpartners.com>
2014-05-07 13:55:32 -0400

    include css for production precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ PopHealth::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+	config.assets.precompile << "*.scss"
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
