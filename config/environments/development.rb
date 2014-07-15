16f78829bdabe47013f1231530094c4d98ad83ca
Sabbir Siddiqui <ssiddiqui@smcpartners.com>
2014-07-15 15:30:38 -0400

    fixed lockable login bugs #2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ PopHealth::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => 'localhost', :port => 3000}
+#  config.action_mailer.raise_delivery_errors = false
+#  config.action_mailer.default_url_options = {:port => 3000}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
